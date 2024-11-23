a08c2d0694292885fec3126b9f4b42284bcf214d
Channveer Mulge <130136525+c-mulge@users.noreply.github.com>
2024-11-23 14:17:28 +0000

    learn operations

--- basic/03_conversion.js
+++ basic/03_conversion.js
@@ -1,5 +1,5 @@
 //Conversion variables
-
+/*
 let score=undefined
 
 console.log(typeof score)
@@ -12,16 +12,45 @@ console.log(valconv)
 //NaN ==>Not a Number
 //true ==>1;  false ==>0
 
-/*
+
 let login=1;
 let val=Boolean(login)
 console.log(typeof val)
 console.log(val)
-*/
 
-/*
+
+
 let strr=123
 let stri=String(strr)
 console.log(typeof stri)
 console.log(stri)
 */
+
+//operations
+
+let value=3
+let negvalue=-value
+
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+// let str1="hello"
+// let str2=" channu"
+// console.log(str1+str2)
+// console.log("1"+"2")
+// console.log(1+2+"2")
+
+// console.log(+true)
+// console.log(true+)         //gives error
+
+
+let count=0
+
+count++
+console.log(count)
