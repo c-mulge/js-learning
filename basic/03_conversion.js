df1b6d270cc2f5f8d6b475dfd1c340a553b6f1d8
Channveer Mulge <130136525+c-mulge@users.noreply.github.com>
2024-11-23 13:50:26 +0000

    conversion

---
+++ basic/03_conversion.js
@@ -0,0 +1,27 @@
+//Conversion variables
+
+let score=undefined
+
+console.log(typeof score)
+let valconv=Number(score)
+
+console.log(typeof valconv)
+console.log(valconv)
+
+
+//NaN ==>Not a Number
+//true ==>1;  false ==>0
+
+/*
+let login=1;
+let val=Boolean(login)
+console.log(typeof val)
+console.log(val)
+*/
+
+/*
+let strr=123
+let stri=String(strr)
+console.log(typeof stri)
+console.log(stri)
+*/
