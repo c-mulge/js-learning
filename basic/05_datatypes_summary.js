75e1a51eb6a466bb82209b6d24df787325938da8
Channveer Mulge <130136525+c-mulge@users.noreply.github.com>
2024-11-24 12:30:09 +0000

    datatypes summary

---
+++ basic/05_datatypes_summary.js
@@ -0,0 +1,40 @@
+//Data types in Java
+
+/*
+
+JavaScript is dynamically typed language
+
+1. Primitive Datatype: 
+String, Number, Boolean, null, undefined, Symbol, BigInt
+
+2. Non-Primitive Datatype(Reference):
+Array, Objects, Functions
+*/
+
+const score =100    //Number
+const str="Hello"  //String
+const bool=true    //Boolean
+const a=null       //null
+let b;            //undefined
+
+const id=Symbol('123')   //Symbol
+let c=664767462346287387n  //BigInt
+console.log(c);
+
+const hero=["spiderman","hulk","thor","captain"]   //array
+
+let myObj={                  //Object
+    name: "Soham",
+    age: 20
+}
+
+const myfun=function(){             //function
+console.log("Hello World");
+
+}
+
+myfun()
+
+console.log(typeof hero);   //typeof ==>object
+console.log(typeof myObj);  //typeof ==>object
+console.log(typeof myfun);
