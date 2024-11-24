928d9f237ab39c4d8f68b62d64e4eca4cb087970
Channveer Mulge <130136525+c-mulge@users.noreply.github.com>
2024-11-24 12:13:34 +0000

    comparison added

---
+++ basic/04_comparison.js
@@ -0,0 +1,17 @@
+//Comparison
+
+// console.log("Hello")
+
+// console.log(2>1)
+// console.log(1>=1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+
+console.log("2">1)    //true
+
+console.log(null>0)   //false
+console.log(null>=0)  //true
+console.log(null==0)  //false
+
+// '===' is a strict check, it also checks the datatype of variable also
