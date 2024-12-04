d769a10cb088bed717bb02667dc409504e55ee03
Channveer Mulge <130136525+c-mulge@users.noreply.github.com>
2024-12-04 13:57:17 +0000

    control_flow

---
+++ control_flow/1.js
@@ -0,0 +1,39 @@
+// if
+
+/*if(condition){
+
+}*/
+
+if(true){
+    // console.log(true);
+}
+
+const isuserloggedin=true
+// if(isuserloggedin){
+// }
+
+//comparison:  >,>=,<,<=,==,!=,=== conditions
+//'===' checks the datatype also
+
+if(2==='2'){
+    console.log("executed");
+}
+
+
+//if-else
+
+/*if(2==3){
+    console.log(`they are equal`);
+}else{
+    console.log(`they are not equal`);
+}*/
+
+/*switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}*/
+
