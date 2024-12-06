a357f669c33d9e8b3777c5db40e4747d7f15029f
Channveer Mulge <130136525+c-mulge@users.noreply.github.com>
2024-12-06 12:52:06 +0000

    loop and practice code

---
+++ Iterators/one.js
@@ -0,0 +1,19 @@
+//for loop
+
+let array=['a','b','c','d','e']
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    // console.log(element);
+    // console.log(array[index]);
+}
+
+for (let i = 0; i <= 10; i++) {
+    for (let j = 0; j <=i; j++) {
+        // const element = array[j];
+        // console.log(i,j);
+        // console.log(i);
+        // console.log(j);
+    }
+}
+
---
+++ practice /p1.js
@@ -0,0 +1,14 @@
+const obj1={
+    firstname: "Sagar",
+    lastname: "Mulge",
+    age: 17,
+    fullname: function(){
+        return this.firstname+ " "+this.lastname
+    }
+}
+
+// console.log(obj1.fullname());
+
+// function obj2(f,l,a){
+
+// }
